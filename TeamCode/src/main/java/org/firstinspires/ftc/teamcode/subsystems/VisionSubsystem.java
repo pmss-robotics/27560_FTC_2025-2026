9afa6a02fdbad97a93263987d8e531d86f94b5e9
alderPikachu <windspickup@gmail.com>
2025-09-11 21:05:39 -0700

    Started on vision. April tag identification can be tested.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionSubsystem.java
@@ -0,0 +1,142 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import static java.lang.Thread.sleep;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.function.Consumer;
+import org.firstinspires.ftc.robotcore.external.function.Continuation;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource;
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Config // Makes variables changeable on the FTC dashboard
+public class VisionSubsystem extends SubsystemBase {
+
+    public static int EXPOSURE_MS = 6;
+    public static int GAIN = 190;
+
+    private AprilTagProcessor aprilTag;
+    private VisionPortal visionPortal;
+    private Telemetry telemetry;
+    public VisionSubsystem(HardwareMap hardwareMap, Telemetry telemetry) throws InterruptedException {
+        this.telemetry = telemetry;
+
+        final CameraStreamProcessor dashboard = new CameraStreamProcessor();
+
+        aprilTag = new AprilTagProcessor.Builder()
+                .setDrawAxes(false)
+                .setDrawCubeProjection(false)
+                .setDrawTagOutline(true)
+                .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                .setTagLibrary(AprilTagGameDatabase.getDecodeTagLibrary())
+                .build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam"));
+
+        builder.addProcessor(aprilTag);
+
+        visionPortal = builder.build();
+
+        visionPortal.setProcessorEnabled(aprilTag, false);
+
+        ElapsedTime elapsedTime = new ElapsedTime();
+        VisionPortal.CameraState cameraState = visionPortal.getCameraState();
+        while(elapsedTime.seconds() < 20 && cameraState != VisionPortal.CameraState.STREAMING){
+            cameraState = visionPortal.getCameraState();
+        }
+        telemetry.log().add("Time from build to streaming " + elapsedTime.milliseconds() + "ms");
+        if (visionPortal.getCameraState() == VisionPortal.CameraState.STREAMING) {
+            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                exposureControl.setMode(ExposureControl.Mode.Manual);
+                sleep(50);
+            }
+            exposureControl.setExposure(EXPOSURE_MS, TimeUnit.MILLISECONDS);
+            sleep(20);
+            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+            gainControl.setGain(GAIN);
+            sleep(20);
+        }
+        FtcDashboard.getInstance().startCameraStream(dashboard, 15);
+
+    }
+
+    @Override
+    public void periodic() {
+        telemetryAprilTag();
+    }
+
+    public void enableDetection(boolean enabled) {
+        visionPortal.setProcessorEnabled(aprilTag, enabled);
+    }
+
+    private void telemetryAprilTag() {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format(Locale.CANADA, "\n==== (ID %d) %s", detection.id, detection.metadata.name));
+            } else {
+                telemetry.addLine(String.format(Locale.CANADA, "\n==== (ID %d) Unknown", detection.id));
+            }
+
+        }
+    }
+
+    public static class CameraStreamProcessor implements VisionProcessor, CameraStreamSource {
+        private final AtomicReference<Bitmap> lastFrame =
+                new AtomicReference<>(Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565));
+
+        @Override
+        public void init(int width, int height, CameraCalibration calibration) {
+            lastFrame.set(Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565));
+        }
+
+        @Override
+        public Object processFrame(Mat frame, long captureTimeNanos) {
+            Bitmap b = Bitmap.createBitmap(frame.width(), frame.height(), Bitmap.Config.RGB_565);
+            Utils.matToBitmap(frame, b);
+            lastFrame.set(b);
+            return null;
+        }
+
+        @Override
+        public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight,
+                                float scaleBmpPxToCanvasPx, float scaleCanvasDensity,
+                                Object userContext) {
+            // do nothing
+        }
+
+        @Override
+        public void getFrameBitmap(Continuation<? extends Consumer<Bitmap>> continuation) {
+            continuation.dispatch(bitmapConsumer -> bitmapConsumer.accept(lastFrame.get()));
+        }
+    }
+}
