635a4af964c3fceabc8cf92066af3115e05ec972
alderPikachu <windspickup@gmail.com>
2025-11-17 21:36:52 -0800

    bare bones TurretSubsystem added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TurretSubsystem.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PwmControl;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+// https://docs.ftclib.org/ftclib/command-base/command-system/subsystems
+@Config
+public class TurretSubsystem extends SubsystemBase {
+
+    // declare hardware here
+    Telemetry telemetry;
+
+    // TODO: Rename these
+    ServoImplEx servo1;
+    ServoImplEx servo2;
+
+    public TurretSubsystem (HardwareMap hardwareMap, Telemetry telemetry) {
+        // initialize hardware here alongside other parameters
+        this.telemetry = telemetry;
+
+
+        // TODO: Rename these
+        servo1 = hardwareMap.get(ServoImplEx.class, "turret1");
+        servo2 = hardwareMap.get(ServoImplEx.class, "turret2");
+
+        servo1.setPwmRange(new PwmControl.PwmRange(500, 2500));
+        servo2.setPwmRange(new PwmControl.PwmRange(500, 2500));
+    }
+
+    @Override
+    public void periodic() {
+        /*
+        Put necessary control flows that need to be updated per command scheduler run
+        Similar functionality can be accomplished through default commands.
+        I think we should limit periodic() to telemetry and other things alike
+         */
+    }
+
+    public void turn(double angle) {
+        servo1.setPosition(scale(angle));
+        servo2.setPosition(scale(angle));
+    }
+
+    private double scale(double angle) {
+        // TODO: Set the range based on the gear ratio of the servos (how many turns the actual turret moves not the servos)
+        return Range.scale(angle, 0, 1800, 0, 1);
+    }
+}
