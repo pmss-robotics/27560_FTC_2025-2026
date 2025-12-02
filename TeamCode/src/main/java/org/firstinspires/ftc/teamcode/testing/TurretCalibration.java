319055b7138aef26124082281f423842b04dc156
alderPikachu <windspickup@gmail.com>
2025-12-01 23:22:03 -0800

    TurretCalibration added - for calibrating turret servos before attaching gears

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TurretCalibration.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.RunCommand;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subsystems.TurretSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.TurretVisionSubsystem;
+import org.firstinspires.ftc.teamcode.util.StateTransfer;
+import org.firstinspires.ftc.teamcode.util.States;
+
+@TeleOp(name = "TurretCalibration", group = "Testing")
+public class TurretCalibration extends CommandOpMode {
+    private GamepadEx driver1;
+    private TurretSubsystem turret;
+
+
+    @Override
+    public void initialize() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        telemetry.log().setDisplayOrder(Telemetry.Log.DisplayOrder.NEWEST_FIRST);
+        telemetry.log().setCapacity(8);
+        driver1 = new GamepadEx(gamepad1);
+
+        turret = new TurretSubsystem(hardwareMap, telemetry);
+
+        new GamepadButton(driver1, GamepadKeys.Button.A)
+                .whenPressed(new InstantCommand(() -> turret.turn(0)));
+
+        new GamepadButton(driver1, GamepadKeys.Button.DPAD_LEFT)
+                .whenPressed(new InstantCommand(() -> turret.turn(turret.getAngle()+5)));
+
+        new GamepadButton(driver1, GamepadKeys.Button.DPAD_RIGHT)
+                .whenPressed(new InstantCommand(() -> turret.turn(turret.getAngle()-5)));
+    }
+}
