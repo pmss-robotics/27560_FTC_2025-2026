69427b0c83d1bbb41df46b1b08825d44d099899f
alderPikachu <windspickup@gmail.com>
2025-12-04 16:18:37 -0800

    Alliance can be set in ftc dashboard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/StateTransfer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/StateTransfer.java
@@ -2,8 +2,10 @@ package org.firstinspires.ftc.teamcode.util;
 
 // import com.pedropathing.localization.Pose;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
 
+@Config
 public class StateTransfer {
     // public static Pose pose = new Pose();
 
